c58f354825fab35079232cf7b8425ce11d1efeb6
Ian-Acosta2330 <ian2330acosta@gmail.com>
2025-09-30 19:36:59 -0600

    working time slots

--- index.php
+++ index.php
@@ -44,6 +44,7 @@
     //  --- JSON SETUP FUNCTIONS ---
 
     //given text file to a more convenient json
+
     function fromTextToJSON(){
         $filename = "patientlist-full.txt";
 
@@ -145,6 +146,7 @@
             "<td>
                 <form method='post' action=''>
                     <label>New?
+                        <input type='hidden' name='is_new' value='0'>
                         <input type='checkbox' name='is_new' value='1'>
                     </label>
                     <input type='hidden' name='action' value='add_schedule'>
@@ -154,10 +156,9 @@
                     <input type='hidden' name='coordinate2' value='" . $patient['coordinate2'] . "'>
                     <input type='hidden' name='day' value='" . $patient['day'] . "'>
                     <input type='hidden' name='month' value='" . $patient['month'] . "'>
-                    <input type='hidden' name='year' value='" . $patient['year'] . "'>
-                    <input type='time' name='start'>
-                    <input type='time' name='end'>
-                    <input type='submit' value='Add To Schedule'>
+                    <input type='hidden' name='year' value='" . $patient['year'] . "'>".
+                    generateAvailableTimes().
+                    "<input type='submit' value='Add To Schedule'>
                 </form>
             </td>";
 
@@ -182,10 +183,49 @@
         return chr($letter+64);
     }
 
+    // see available times: quarterly and a boolean
+function generateAvailableTimes() {
+    $startTime = 9;   
+    $endTime   = 17;//offset by one since day ends at 6pm
+    $quarterly = 15;
+
+    $filename = 'availableTimes.json';
+
+    if (file_exists($filename)) {
+        $data = file_get_contents($filename);
+        $content = $data ? json_decode($data, true) : [];
+    } else {
+        $content = [];
+
+        for ($hour = $startTime; $hour <= $endTime; $hour++) {
+            for ($min = 0; $min < 60; $min += $quarterly) {
+                $timeString = sprintf("%02d:%02d", $hour, $min);
+                $content[$timeString] = ["taken" => false];
+            }
+        }
+        file_put_contents($filename, json_encode($content, JSON_PRETTY_PRINT));
+    }
+    // make dropdown
+    $availabilityDropdown = "
+        <label for='avail'>Select Time:</label>
+        <select name='avail' id='avail'>
+    ";
+
+    foreach ($content as $time => $info) {
+        if($time)
+        $availabilityDropdown .= "<option value='{$time}'>{$time}</option>";
+    }
+
+    $availabilityDropdown .= "
+        </select>
+        <input type='hidden' name='start'>
+    ";
+
+    return $availabilityDropdown;
+}
     //------------------------------------------
 
     //  --- LOGIC METHODS ---
-
     function addPatient($action,$f){
         if($action=='add_pat'){
             $data = file_get_contents($f);
@@ -195,19 +235,66 @@
             list($day,$month,$year) = explode("/",$birthday);
 
             $patient = [
-                "name"       => htmlspecialchars($_POST["addName"]),
-                "address"    => htmlspecialchars($_POST["addAddress"]),
+                "name"=> htmlspecialchars($_POST["addName"]),
+                "address"=> htmlspecialchars($_POST["addAddress"]),
                 "coordinate1"=> ord(strtoupper($_POST["addCoords"])[0]) - 64,
                 "coordinate2"=> substr($_POST["addCoords"], 1),
-                "day"        => $day,
-                "month"      => $month,
-                "year"       => $year
+                "day"=> $day,
+                "month"=> $month,
+                "year"=> $year
             ];
             $patients[] = $patient;
             file_put_contents($f, json_encode($patients, JSON_PRETTY_PRINT));
         } 
 
     }
+
+    function validAppointment($start, $startMin, $startHour, $endMin, $endHour) {
+        $isValid = true;
+        $f = 'availableTimes.json';
+        if(file_exists($f)){
+            $data = json_decode(file_get_contents($f), true);
+            if($data[$start]['taken']===true){
+                $isValid=false;
+            } else {
+                for($checkHour = $startHour; $checkHour <= $endHour; $checkHour++){
+                    $minStart = ($checkHour == $startHour) ? $startMin : 0;
+                    $minEnd   = ($checkHour == $endHour) ? $endMin : 45;
+                    for($checkMin=$minStart;$checkMin <= $minEnd;$checkMin+=15){
+                        if($data[$start]['taken']===true){
+                            $isValid=false;
+                            break 2;
+                        }
+                    }
+                }
+            }
+        }
+        return $isValid;
+    }
+
+    function markAppointmentTaken($startHour, $startMin, $endHour, $endMin) {
+        $f = 'availableTimes.json';
+        $inc = 15;
+
+        if (file_exists($f)) {
+            $data = json_decode(file_get_contents($f), true);
+
+            for ($h = $startHour; $h <= $endHour; $h++) {
+                $minStart = ($h == $startHour) ? $startMin : 0;
+                $minEnd   = ($h == $endHour) ? $endMin : 45;
+
+                for ($m = $minStart; $m <= $minEnd; $m += $inc) {
+                    $slotKey = sprintf("%02d:%02d", $h, $m);
+                    if (isset($data[$slotKey])) {
+                        $data[$slotKey]['taken'] = true;
+                    }
+                }
+            }
+
+            file_put_contents($f, json_encode($data, JSON_PRETTY_PRINT));
+        }
+    }
+
 
     function addToSchedule($action){
         $filename = 'schedule.json';
@@ -219,6 +306,39 @@
                 $content = [];
             }
     
+            $start = ($_POST['avail']);
+            $end = "";
+
+            $startHour= (int)strtok($start,':');
+            $startMin = (int)strtok(':');
+            $endHour = $startHour;
+            $endMin = $startMin;
+
+            $new = $_POST['is_new'];
+
+            if(boolval($new)){
+                $endMin = $startMin;
+                $endHour = $startHour+1;
+            } else{
+                $endMin = $endMin + 30;
+                if($endMin>60){
+                    $endHour = $startHour+1;
+                    $endMin = $endMin % 60;
+                }
+            }
+            if(strlen($endMin) < 2){
+                $endMin.='0';
+            }
+            $end = $endHour.":".$endMin;
+
+            if(!(validAppointment($start, $startMin, $startHour, $endMin, $endHour))){   
+                echo 
+                "<script type='text/javascript'>".
+                "alert('* TAKEN *');</script>";
+                return;            
+            }
+
+            markAppointmentTaken($startHour, $startMin, $endHour, $endMin);
             $entry = [
                 "name"=>htmlspecialchars($_POST['name']),
                 "address"=>htmlspecialchars($_POST['address']),
@@ -227,8 +347,8 @@
                 "day"=>htmlspecialchars($_POST['day']),
                 "month"=>htmlspecialchars(($_POST['month'])),
                 "year"=>htmlspecialchars(($_POST['year'])),
-                "availableStart"=>htmlspecialchars(($_POST['start'])),
-                "availableEnd"=>htmlspecialchars(($_POST['end'])),
+                "availableStart"=>htmlspecialchars($start),
+                "availableEnd"=>htmlspecialchars($end),
                 "new"=>htmlspecialchars($_POST['is_new']),
             ];
 
@@ -252,19 +372,27 @@
         "<th scope='col'>Date</th>".
         "<th scope='col'>New Patient?</th>".
         "<th scope='col'>Times Available</th>".
+        "<th scope='col'>Duration</th>".
         "</tr>"."</thead>";
         echo "<tbody>";
         foreach ($patients as $patient) {
+            $duration = ($patient['new'] == "1") ? 60 : 30;
             echo "<tr>".
             "<th scope='row'>".htmlspecialchars($patient['name'])."</th>".
             "<td>". htmlspecialchars($patient['address'])."</td>".
             "<td>".toLetter($patient['coordinate1'])." ".$patient['coordinate2'] ."</td>".
             "<td>".$patient['day'] . "/" . $patient['month']."/" .$patient['year'] ."</td>".
-            "<td>".boolval($patient['new'])."</td>".
-            "<td>".$patient['availableStart']."-". $patient['availableEnd']."</td>";
+            "<td>".($patient['new'] == "1" ? "Yes" : "No")."</td>".
+            "<td>".$patient['availableStart']."-". $patient['availableEnd']."</td>".
+            "<td>".$duration." min</td>";
+
             echo "</tr>";
         }
         echo "</div>";
+    }
+
+    function makeList(){
+
     }
 
     //MAIN -- Function Calls
